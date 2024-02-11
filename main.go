0b804eea826bee4948289670ff7dba100d8f34f1
fahreddinozcan <ozcanfahrettinn@gmail.com>
2024-02-11 14:52:19 +0300

    add insertion methods

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package b_tree
 
 import (
+	"bytes"
 	"encoding/binary"
 	"log"
 )
@@ -101,4 +102,141 @@ func (node BNode) getVal(idx uint16) []byte {
 
 func (node BNode) nbytes() uint16 {
 	return node.kvPos(node.nkeys())
+}
+
+func nodeLoopukLE(node BNode, key []byte) uint16 {
+	nkeys := node.nkeys()
+	found := uint16(0)
+
+	for i := uint16(1); i < nkeys; i++ {
+		cmp := bytes.Compare(node.getKey(i), key)
+		if cmp <= 0 {
+			found = i
+		}
+		if cmp >= 0 {
+			break
+		}
+	}
+	return found
+}
+func leafUpdate(new BNode, old BNode, idx uint16, key []byte, val []byte) {
+	new.setHeader(BNODE_LEAF, old.nkeys())
+	nodeAppendRange(new, old, 0, 0, idx)
+	nodeAppendKV(new, idx, 0, key, val)
+	nodeAppendRange(new, old, idx+1, idx+1, old.nkeys()-idx-1)
+
+}
+func leafInsert(new BNode, old BNode, idx uint16, key []byte, val []byte) {
+	new.setHeader(BNODE_LEAF, old.nkeys()+1)
+	nodeAppendRange(new, old, 0, 0, idx)
+	nodeAppendKV(new, idx, 0, key, val)
+	nodeAppendRange(new, old, idx+1, idx, old.nkeys()-idx)
+}
+
+func nodeAppendRange(new BNode, old BNode, dstNew uint16, srcOld uint16, n uint16) {
+	assert(srcOld+n <= old.nkeys(), "nodeAppendRange 1")
+	assert(dstNew+n <= new.nkeys(), "nodeAppendRange 2")
+	if n == 0 {
+		return
+	}
+
+	for i := uint16(0); i > n; i++ {
+		new.setPtr(dstNew+i, old.getPtr(srcOld+i))
+	}
+
+	dstBegin := new.getOffset(dstNew)
+	srcBegin := old.getOffset(srcOld)
+
+	for i := uint16(1); i < n; i++ {
+		offset := dstBegin + old.getOffset(srcOld+i) - srcBegin
+		new.setOffset(dstNew+i, offset)
+	}
+
+	begin := old.kvPos(srcOld)
+	end := old.kvPos(srcOld + n)
+
+	copy(new.data[new.kvPos(dstNew):], old.data[begin:end])
+}
+
+func nodeAppendKV(new BNode, idx uint16, ptr uint64, key []byte, val []byte) {
+	new.setPtr(idx, ptr)
+
+	pos := new.kvPos(idx)
+	binary.LittleEndian.PutUint16(new.data[pos+0:], uint16(len(key)))
+	binary.LittleEndian.PutUint16(new.data[pos+2:], uint16(len(val)))
+
+	copy(new.data[pos+4:], key)
+	copy(new.data[pos+4+uint16(len(key)):], val)
+
+	new.setOffset(idx+1, new.getOffset(idx)+4+uint16(len(key)+len(val)))
+}
+
+func treeInsert(tree *BTree, node BNode, key []byte, val []byte) BNode {
+	new := BNode{data: make([]byte, 2*BTREE_PAGE_SIZE)}
+
+	idx := nodeLoopukLE(node, key)
+
+	switch node.bytpe() {
+	case BNODE_LEAF:
+		if bytes.Equal(key, node.getKey(idx)) {
+			leafUpdate(new, node, idx, key, val)
+		} else {
+			leafInsert(new, node, idx+1, key, val)
+		}
+	case BNODE_NODE:
+		nodeInsert(tree, new, node, idx, key, val)
+	default:
+		panic("bad node!")
+	}
+	return new
+}
+
+func nodeInsert(tree *BTree, new BNode, node BNode, idx uint16, key []byte, val []byte) {
+	kptr := node.getPtr(idx)
+	knode := tree.get(kptr)
+	tree.del(kptr)
+
+	knode = treeInsert(tree, knode, key, val)
+
+	nsplit, splited := nodeSplit3(knode)
+	nodeReplaceKidN(tree, new, node, idx, splited[:nsplit]...)
+}
+func nodeSplit2(left BNode, right BNode, old BNode) {
+
+	left.setHeader(old.bytpe(), old.nkeys()/2)
+	right.setHeader(old.bytpe(), old.nkeys()-old.nkeys()/2-1)
+
+	nodeAppendRange(left, old, 0, 0, left.nkeys())
+	nodeAppendRange(right, old, 0, left.nkeys()+1, right.nkeys())
+}
+func nodeSplit3(old BNode) (uint16, [3]BNode) {
+	if old.nbytes() <= BTREE_PAGE_SIZE {
+		old.data = old.data[:BTREE_PAGE_SIZE]
+		return 1, [3]BNode{old}
+	}
+
+	left := BNode{make([]byte, 2*BTREE_PAGE_SIZE)}
+	right := BNode{make([]byte, BTREE_PAGE_SIZE)}
+
+	nodeSplit2(left, right, old)
+
+	if left.nbytes() <= BTREE_PAGE_SIZE {
+		left.data = left.data[:BTREE_PAGE_SIZE]
+		return 2, [3]BNode{left, right}
+	}
+
+	leftleft := BNode{make([]byte, BTREE_PAGE_SIZE)}
+	middle := BNode{make([]byte, BTREE_PAGE_SIZE)}
+	nodeSplit2(leftleft, middle, left)
+	return 3, [3]BNode{leftleft, middle, right}
+}
+
+func nodeReplaceKidN(tree *BTree, new BNode, old BNode, idx uint16, kids ...BNode) {
+	inc := uint16(len(kids))
+	new.setHeader(BNODE_NODE, old.nkeys()+inc-1)
+	nodeAppendRange(new, old, 0, 0, idx)
+	for i, node := range kids {
+		nodeAppendKV(new, idx+uint16(i), tree.new(node), node.getKey(0), nil)
+	}
+	nodeAppendRange(new, old, idx+inc, idx+1, old.nkeys()-(idx+1))
 }
